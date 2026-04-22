31cd894fee73290a702ca813edca1e75160da150
pdtosky <pdtosky@gmail.com>
2026-04-22 15:57:19 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.APP_CONFIG = {
   supabaseUrl: "https://fftdjnjnvusgrbbfbwcw.supabase.co",
   supabaseAnonKey: "sb_publishable_gh-Sk00ag9txoBF5Y5u5sQ_ybAY3jL-",
   supabaseTable: "app_state",
-  supabaseRowId: "main"
+  supabaseRowId: "main",
+  adminEmails: ["tape@jhint.net"]
 };
